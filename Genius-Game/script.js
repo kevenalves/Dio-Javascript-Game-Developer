8263b0370cf4568dfdacd76af308e7ab654f5e20
keven <kevenalvesm@gmail.com>
2022-03-30 22:14:49 -0300

    att menu inicial, pontuacao, mensagens

--- Genius-Game/script.js
+++ Genius-Game/script.js
@@ -18,7 +18,7 @@ let shuffleOrder = () => {
     order[order.length] = colorOrder;
     clickedOrder = [];
 
-    for(let i in order) {
+    for (let i in order) {
         let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number(i) + 1);
     }
@@ -36,30 +36,40 @@ let lightColor = (element, number) => {
 }
 
 //checa se os botoes clicados são os mesmos da ordem gerada no jogo
+
 let checkOrder = () => {
-    for(let i in clickedOrder) {
-        if(clickedOrder[i] != order[i]) {
+    for (let i in clickedOrder) {
+        if (clickedOrder[i] != order[i]) {
             gameOver();
             break;
         }
     }
-    if(clickedOrder.length == order.length && order.length <= 5) {
-        alert(`Você acertou!\nPontuação: ${score}\nClique em Ok para iniciar o próximo nível!`);
+    if (clickedOrder.length == order.length && order.length <= 4) {
         nextLevel();
-    } else if(clickedOrder.length == order.length && order.length > 5 && order.length <= 9 ) {
-        alert(`INCRÍVELLL!\nVocê acertou: ${score} rodadas!\nClique em Ok para iniciar o próximo nível!`);
+    } else if (clickedOrder.length == order.length && order.length == 5) {
+        msg1();
         nextLevel();
-    } else if(clickedOrder.length == order.length && order.length > 9 && order.length <= 14) {
-        alert(`INACREDITÁVELLLLLLL!\nVocê acertou: ${score} rodadas!\nPreparado(a)?\nClique em Ok para iniciar o próximo nível!`);
+    } else if (clickedOrder.length == order.length && order.length > 5 && order.length <= 9) {
         nextLevel();
-    } else if(clickedOrder.length == order.length && order.length > 14 && order.length <= 39) {
-        alert(`MEU DEUSSSS!!! INACREDITÁVELLLLLLL!\nVocê acertou: ${score} rodadas!\nClique em Ok para iniciar o próximo nível!`);
+    } else if (clickedOrder.length == order.length && order.length == 10) {
+        msg2();
         nextLevel();
-    } else if(clickedOrder.length == order.length && order.length > 39 && order.length <= 99) {
-        alert(`VOCÊ É UM DEUS!!! Que memória absurda!\nVocê acertou: ${score} rodadas!\nClique em Ok para iniciar o próximo nível!`);
+    } else if (clickedOrder.length == order.length && order.length > 10 && order.length <= 19) {
         nextLevel();
-    } else if(clickedOrder.length == order.length && order.length > 100) {
-        alert(`O que você ainda está fazendo aqui?! SEU LOUCOOOOOOOO!\nVocê acertou: ${score} rodadas!\nClique em Ok para iniciar o próximo nível!`);
+    } else if (clickedOrder.length == order.length && order.length == 20) {
+        msg3();
+        nextLevel();
+    } else if (clickedOrder.length == order.length && order.length > 20 && order.length <= 49) {
+        nextLevel();
+    } else if (clickedOrder.length == order.length && order.length == 50) {
+        msg4();
+        nextLevel();
+    } else if (clickedOrder.length == order.length && order.length > 50 && order.length <= 99) {
+        nextLevel();
+    } else if (clickedOrder.length == order.length && order.length == 100) {
+        msg5();
+        nextLevel();
+    } else if (clickedOrder.length == order.length && order.length > 100) {
         nextLevel();
     }
 }
@@ -72,14 +82,14 @@ let click = (color) => {
     setTimeout(() => {
         createColorElement(color).classList.remove('active');
         checkOrder();
-    },250);
+    }, 250);
 }
 
 //funcao que retorna a cor
 let createColorElement = (color) => {
-    if(color == 0) {
+    if (color == 0) {
         return green;
-    } else if(color == 1) {
+    } else if (color == 1) {
         return red;
     } else if (color == 2) {
         return purple;
@@ -92,11 +102,12 @@ let createColorElement = (color) => {
 let nextLevel = () => {
     score++;
     shuffleOrder();
+    attScore();
 }
 
 //funcao para game over
 let gameOver = () => {
-    alert(`Pontuação: ${score}\nVocê perdeu!\nClique em OK para iniciar um novo jogo!`);
+    alert(`Pontuação final: ${score}\nVocê perdeu! :(\nClique em OK para iniciar um novo jogo!`);
     order = [];
     clickedOrder = [];
 
@@ -105,7 +116,6 @@ let gameOver = () => {
 
 //funcao de inicio do jogo
 let playGame = () => {
-    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
     score = 0;
 
     nextLevel();
@@ -118,5 +128,96 @@ purple.onclick = () => click(2);
 blue.onclick = () => click(3);
 
 
-//inicio do jogo
-playGame();
+//Menu do jogo
+const menu = document.querySelector('.menu');
+
+function iniciar() {
+    menu.classList.add('none');
+
+    setTimeout(() => {
+        playGame();
+        attScore();
+    }, 200);
+}
+
+//Atualiza o número da rodada
+const point = document.querySelector('.point');
+
+function attScore() {
+    point.innerHTML = (score)
+}
+
+
+
+// === Mensagens personalizadas === //
+const container = document.getElementById("container");
+
+// 5 rodadas seguidas
+function msg1() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = `INCRÍVEL!\nVocê acertou: ${score} rodadas seguidas!!!\n`;
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 6000);
+}
+
+// 10 rodadas seguidas
+function msg2() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = `INACREDITÁVEL!\nVocê acertou: ${score} rodadas seguidas!!!\n`;
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 6000);
+}
+
+// 20 rodadas seguidas
+function msg3() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = `MEU DEUS, ISSO É LOUCURA!\nVocê acertou: ${score} rodadas seguidas!!!\n`;
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 6000);
+}
+
+// 50 rodadas seguidas
+function msg4() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = `VOCÊ AINDA É HUMANO?\nVocê acertou: ${score} rodadas seguidas!!!\n`;
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 6000);
+}
+
+// 100 rodadas seguidas
+function msg5() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = `VOCÊ É UM DEUS!\nVocê acertou: ${score} rodadas seguidas!!!\n`;
+
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 6000);
+}
